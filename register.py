ac1876e7f57c5299fed3d6d7a3880fc7281bca71
Kameron Arceneaux <kameronarceneaux05@Gmail.com>
2021-08-27 06:40:40 -0500

    created empty register screen

--- register.py
+++ register.py
@@ -1,2 +1,12 @@
 from tkinter import *
 from utlities import information
+import json
+
+
+class Register(Toplevel):
+    def __init__(self):
+        Toplevel.__init__(self)
+        self.title("InPocket -- Money Manager")
+        self.geometry("650x550+600+250")
+        self.resizable(False, False)
+        self.mainloop()
