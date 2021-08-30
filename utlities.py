d3640e6e4bdbd8930f6062c344cf5809d78a211a
Kameron Arceneaux <kameronarceneaux05@Gmail.com>
2021-08-29 20:32:53 -0500

    added money quotes

--- utlities.py
+++ utlities.py
@@ -4,3 +4,18 @@ information = {
     "titleText": "#355040",
     "thirdOfFrame": 0.3 * 500,
 }
+
+FONTTEXTCOLOR = "#355040"
+
+random_money_quotes = [
+    "“The bitterness of poor quality remains long after the sweetness of low price is forgotten.” – Benjamin Frankin",
+    "“Empty pockets never held anyone back. Only empty heads and empty hearts can do that.” – Norman Vincent Peale ",
+    "“Saving must become a priority, not just a thought. Pay yourself first.” – Dave Ramsey",
+    "“Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.” – Ayn Rand",
+    "“Everyday is a bank account, and time is our currency. No one is rich, no one is poor, we’ve got 24 hours each.” – Christopher Rice",
+    "“There is a gigantic difference between earning a great deal of money and being rich.” – Marlene Dietrich",
+    "“Enough is better than too much.” Dutch Proverb ",
+    "“Never spend your money before you have it.” – Thomas Jefferson",
+    "“Saving is the gap between your ego and your income. ― Morgan Housel, The Psychology of Money",
+    "The price of anything is the amount of life you exchange for it. – Henry David Thoreau",
+]
